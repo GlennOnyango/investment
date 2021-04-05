6b075c80f940ff261f2c0a7e8dbc02a2fcc5d32d
Glenn <gonyango@>
2021-04-05 18:07:04 +0300

    Global banking

--- js/api.js
+++ js/api.js
@@ -2,7 +2,7 @@ var reg_form = document.querySelector("#reg_form");
 var log_form = document.querySelector("#log_form");
 var for_form = document.querySelector("#for_form");
 
-reg_form.addEventListener("submit",(e)=>{
+/*reg_form.addEventListener("submit",(e)=>{
   e.preventDefault();
   const formData = new FormData(reg_video);
 
@@ -24,4 +24,21 @@ for_form.addEventListener("submit",(e)=>{
 
 
   console.log("done");
+});
+
+*/
+var change_the_page = document.querySelectorAll("#change_the_page");
+var show_page = document.querySelectorAll(".show_page");
+change_the_page.forEach(element => {
+  element.addEventListener("click",()=>{
+    
+show_page.forEach(elem => {
+  console.log(elem.id);
+  if (elem.id == element.classList[2] ){
+    elem.classList.remove("d-none");
+  }else{
+    elem.classList.add("d-none");
+  }
+});
+  });
 });
